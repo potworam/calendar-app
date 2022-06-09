9e8dc23ce799181cf08b86ea687637f99ad89ddc
Potwora <matthewpotwora@yahoo.com>
2022-06-08 21:36:42 -0700

    altered the README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # calendar-app
+this is my calendar app it is supposed to be a planner for your events of the day,and change color based on the hour.
+
+heres a link to the website: https://potworam.github.io/calendar-app/
+
+heres a photo of the website: ![website pic](./Screen%20Shot%202022-06-08%20at%209.25.35%20PM.png)
--- script.js
+++ script.js
@@ -8,17 +8,24 @@ parElement.appendChild(textToAdd);
  //above text gets my date function
 
  let currentHour = moment().format("h:mm:ss");
+console.log(currentHour) 
+$(".time-div").each(function(){
+ var timeDiv = $(this).attr("id");
 
-//  var timeDiv = $(this).attr("id");
+    if (currentHour == timeDiv) {
+        $(this).addClass("present");
+    }else if (currentHour < timeDiv) {
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    }else if (currentHour > timeDiv) {
+        $(this).removeClass("past");
+        $(this).addClass("future");
+    };
+});
+document.querySelector('span').addEventListener(click)
 
-//     if (currentHour == timeDiv) {
-//         $(this).addclass("present");
-//     }else if (currentHour < timeDiv) {
-//         $(this).removeClass ("present");
-//         $(this).addClass("future");
-//     }else if (currentHour > timeDiv) {
-//         $(this).removeClass ("future");
-//         $(this).addclass("past");
-//     };
-
-    
+document.querySelector('button').onclick = function(event) {
+    var a = event.target.innerHTML;
+    console.log(a);
+  
+  }
